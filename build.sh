7ea931e56cf52e3c0b11491ce0311f22ebf0c7eb
umar <uzaff55@gmail.com>
2025-05-18 12:52:46 -0500

    s

--- build.sh
+++ build.sh
@@ -2,19 +2,14 @@
 # exit on error
 set -o errexit
 
-# Upgrade pip and install build dependencies
+# Install Python dependencies
 python -m pip install --upgrade pip
-python -m pip install --upgrade setuptools wheel
-
-# Install gunicorn first
-pip install gunicorn
-
-# Install other requirements
 pip install -r requirements.txt
 
-# Create build directory if it doesn't exist
+# Create necessary directories and copy files
 mkdir -p build
 cp -r api build/
 cp -r templates build/
 cp -r predict.py build/
+cp -r model.pth build/ # Ensure model file is copied
 cp requirements.txt build/ 
